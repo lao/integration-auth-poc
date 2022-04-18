b6469dfb5ffe9e000800dee0659738282e043d8e
LAO <lucas.oliveira@wetransfer.com>
2022-04-18 14:11:25 -0300

    Add integration bridge architecture diagram

--- server/src/path-fix-integrations.controller.ts
+++ server/src/path-fix-integrations.controller.ts
@@ -9,29 +9,11 @@ export class PathFixIntegrationsController {
 
   @Get('authorize/:service')
   async getAuthorize(@Req() req, @Res() res): Promise<any> {
-    const avilableServices = this.appService.availableServices;
-    const { service } = req.params;
-
-    if (service in avilableServices) {
-      const authorizeUrl = avilableServices[service].getAuthorizeUrl();
-      console.log(authorizeUrl)
-      res.redirect(authorizeUrl);
-    } else {
-      res.status(404).send('Service not found');
-    }
+    // todo
   }
 
   @Post('oauth2/token/:service')
   async getToken(@Req() req, @Res() res): Promise<any> {
-    const avilableServices = this.appService.availableServices;
-    const { service } = req.params;
-
-    if (service in avilableServices) {
-      const token = await avilableServices[service].getToken(req.originalUrl);
-      console.log(token);
-      res.send(token);
-    } else {
-      res.status(404).send('Service not found');
-    }
+    // todo
   }
 }
